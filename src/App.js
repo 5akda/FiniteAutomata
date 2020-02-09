e8326f1c7af08b8939faba0d2435870ce5fb67bf
parzival48 <sakdakluy@hotmail.com>
2020-02-10 01:55:21 +0700

    test web

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          NFA and DFA 55555555
+          Finite Automaton is Loading...
         </p>
       </header>
     </div>
