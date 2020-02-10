99d701ca0e1e6aaa0be75f9dee2acc6947d17c34
parzival48 <sakdakluy@hotmail.com>
2020-02-11 00:01:49 +0700

    try web dev

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Finite Automaton is Loading...
-        </p>
-      </header>
+    <div>
+
+      <div className="Layout-header">
+        Finite Automata - Ticket Vending Machine
+      </div>
+
+      <div className="Layout-input">
+        Input String:
+        <input className="App-input" size="20"/>
+        <button className="App-enterbutton">Enter</button>
+        <button className="App-clearbutton">Clear</button>
+      </div>
+
+      <div className="Layout-body">
+
+      </div>
+
     </div>
   );
 }
