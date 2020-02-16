aeec75578db7ce9c6597653802316bb61f8f5416
parzival48 <sakdakluy@hotmail.com>
2020-02-16 22:21:41 +0700

    core func success

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Component } from 'react'
+import React, { Component } from 'react'
 import './App.css'
 
 import base from './images/base.png'
@@ -34,9 +34,9 @@ import more2coins123 from './images/2more-123.png'
 import more2coinsc from './images/2more-c.png'
 import more2coinsempty from './images/2more-empty.png'
 
-import more1coins123 from './images/1more-123.png'
-import more1coinsc from './images/1more-c.png'
-import more1coinsempty from './images/1more-empty.png'
+import more1coin123 from './images/1more-123.png'
+import more1coinc from './images/1more-c.png'
+import more1coinempty from './images/1more-empty.png'
 
 import done1 from './images/done-1.png'
 import done2 from './images/done-2.png'
@@ -66,7 +66,7 @@ class App extends Component {
 
   }
 
-  handleEnterClick(){
+  handleEnterClick() {
     this.handleRestartClick()
     this.setState({ 
       string: ' '+this.state.text,
@@ -75,51 +75,66 @@ class App extends Component {
     })
   }
 
-  handleNextClick(){
+  handleNextClick() {
     if(this.state.nextbutton === 'Restart'){
       this.handleRestartClick()
     }
     else{
-        this.setState({ nextbutton: 'Next' })
+      this.setState({ nextbutton: 'Next' })
+
+      var presentChar = null
 
       if(this.state.string.length > 1) {
-        let presentChar = this.state.string.charAt(1)
-        this.setState({ string: this.state.string.substring(1) })
-        switch(this.state.nextState){
-          case 's_init': {
-            this.s_init(presentChar)
-            break
-          }
-          case 's_trap': {
-            this.s_trap(presentChar)
-            break
-          }
-        }
+        presentChar = this.state.string.charAt(1).toUpperCase()
       }
-
-      else if(this.state.string.length === 1) {
-        let end = ''
-        this.setState({ string: this.state.string.substring(1) })
-        switch(this.state.nextState){
-          case 's_init': {
-            this.s_init(end)
-            break
-          }
-          case 's_trap': {
-            this.s_trap(end)
-            break
-          }
-        }
+      else if(this.state.string.length === 1){
+        presentChar = ' '
       }
-
       else if(this.state.string.length === 0) {
         this.setState({ nextbutton: 'Restart' })
+        return 0
+      }
+
+      this.setState({ string: this.state.string.substring(1) })
+      // State Selector
+      switch(this.state.nextState){
+        case 's_init': {
+          this.s_init(presentChar)
+          break
+        }
+        case 's_trap': {
+          this.s_trap(presentChar)
+          break
+        }
+        case 's_station1': {
+          this.s_station1(presentChar)
+          break
+        }
+        case 's_station2': {
+          this.s_station2(presentChar)
+          break
+        }
+        case 's_station3': {
+          this.s_station3(presentChar)
+          break
+        }
+        case 's_done': {
+          this.s_done(presentChar)
+          break
+        }
+        case 's_2morecoins': {
+          this.s_2morecoins(presentChar)
+          break
+        }
+        case 's_1morecoin': {
+          this.s_1morecoin(presentChar)
+          break
+        }
       }
     }
-    return(this.state.string.length)
   }
 
-  handleRestartClick(){
+  handleRestartClick() {
     this.setState({
       string: ' '+this.state.text,
       nextState: 's_init',
@@ -129,7 +144,7 @@ class App extends Component {
     clearInterval(this.state.looper)
   }
 
-  handleClearClick(){
+  handleClearClick() {
     this.setState({
       text: '',
       string: '',
@@ -141,7 +156,7 @@ class App extends Component {
     clearInterval(this.state.looper)
   }
 
-  handleAutoplayClick(){
+  handleAutoplayClick() {
     this.setState({looper: setInterval(() => this.handleNextClick(),1000)})
   }
 
@@ -184,6 +199,140 @@ class App extends Component {
     }
   }
 
+  s_station1(nextChar) {
+    switch(nextChar) {
+      case '1': {
+        this.setState({ img: s11 })
+        break
+      }
+      case '2': {
+        this.setState({ img: s12, nextState: 's_station2' })
+        break
+      }
+      case '3': {
+        this.setState({ img: s13, nextState: 's_station3' })
+        break
+      }
+      case ' ': {
+        this.setState({ img: s1empty })
+        break
+      }
+      default: {
+        this.setState({ img: s1c, nextState: 's_done' })
+        break
+      }
+    }
+  }
+
+  s_station2(nextChar) {
+    switch(nextChar) {
+      case '1': {
+        this.setState({ img: s21, nextState: 's_station1' })
+        break
+      }
+      case '2': {
+        this.setState({ img: s22 })
+        break
+      }
+      case '3': {
+        this.setState({ img: s23, nextState: 's_station3' })
+        break
+      }
+      case ' ': {
+        this.setState({ img: s2empty })
+        break
+      }
+      default: {
+        this.setState({ img: s2c, nextState: 's_1morecoin' })
+        break
+      }
+    }
+  }
+
+  s_station3(nextChar) {
+    switch(nextChar) {
+      case '1': {
+        this.setState({ img: s31, nextState: 's_station1' })
+        break
+      }
+      case '2': {
+        this.setState({ img: s32, nextState: 's_station2' })
+        break
+      }
+      case '3': {
+        this.setState({ img: s33 })
+        break
+      }
+      case ' ': {
+        this.setState({ img: s3empty })
+        break
+      }
+      default: {
+        this.setState({ img: s3c, nextState: 's_2morecoins' })
+        break
+      }
+    }
+  }
+
+  s_done(nextChar) {
+    switch(nextChar) {
+      case '1': {
+        this.setState({ img: done1, nextState: 's_station1' })
+        break
+      }
+      case '2': {
+        this.setState({ img: done2, nextState: 's_station2' })
+        break
+      }
+      case '3': {
+        this.setState({ img: done3, nextState: 's_station3' })
+        break
+      }
+      case ' ': {
+        this.setState({ img: doneempty })
+        break
+      }
+      default: {
+        this.setState({ img: donec, nextState: 's_trap' })
+        break
+      }
+    }
+  }
+
+  s_2morecoins(nextChar) {
+    switch(nextChar) {
+      case 'C': {
+        this.setState({ img: more2coinsc, nextState: 's_1morecoin' })
+        break
+      }
+      case ' ': {
+        this.setState({ img: more2coinsempty })
+        break
+      }
+      default: {
+        this.setState({ img: more2coins123, nextState: 's_trap' })
+        break
+      }
+    }
+  }
+
+  s_1morecoin(nextChar){
+    switch(nextChar) {
+      case 'C': {
+        this.setState({ img: more1coinc, nextState: 's_done' })
+        break
+      }
+      case ' ': {
+        this.setState({ img: more1coinempty })
+        break
+      }
+      default: {
+        this.setState({ img: more1coin123, nextState: 's_trap' })
+        break
+      }
+    }
+  }
+
   render() {
     return (
       <div className="Layout-all">
@@ -204,6 +353,12 @@ class App extends Component {
           <button className="clearbutton"
             onClick={ ()=>this.handleClearClick() }>Clear
           </button>
+          <div className="help">
+            <button disabled>1 : Select Station 1</button>
+            <button disabled>2 : Select Station 2</button>
+            <button disabled>3 : Select Station 3</button>
+            <button disabled>C : Insert 1 Coin</button>
+          </div>
         </div>
 
         <div className="Layout-controller">
@@ -220,7 +375,7 @@ class App extends Component {
         </div>
 
         <div className="Layout-body">
-          <img src={this.state.img} alt="img"/>
+          <img src={ this.state.img } alt="img"/>
         </div>
 
       </div>
